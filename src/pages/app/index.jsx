94bac257a132f4c985c08fae71a0e18d29cff87e
arthur044 <arthuralima.com.br@gmail.com>
2024-07-30 11:54:59 -0300

    initial commit

---
+++ src/pages/app/index.jsx
@@ -0,0 +1,24 @@
+import './index.scss'
+
+function App() {
+  return (
+    <div className="pagina-freis">
+      <header>
+        <div className='img-frei'>
+          <img src="/assets/images/logo.svg" alt="" />
+          <h3>React FreiS</h3>
+        </div>
+        <nav className='nav'>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre</li>
+          </ul>
+        </nav>
+      </header>
+      
+
+    </div>
+  );
+}
+
+export default App;
